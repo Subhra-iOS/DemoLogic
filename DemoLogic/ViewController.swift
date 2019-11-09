936e8c580b08f32f81042696d5a35ca14b184717
Subhra Roy <subhra.roy@e-arc.com>
2019-11-09 15:24:56 +0530

    ok

---
+++ DemoLogic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoLogic
+//
+//  Created by Subhra Roy on 09/11/19.
+//  Copyright © 2019 Subhra Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
