4bce8019ed7618f3ac7504ea9092cae8ed18a327
Subhra Roy <subhra.roy@e-arc.com>
2019-11-09 20:33:08 +0530

    ok

--- DemoLogic/ViewController.swift
+++ DemoLogic/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        createTriangleWith(lineNo: 10)
+//        createTriangleWith(lineNo: 10)
+//        createRightfaceTriangleWith(lineNo: 10)
+//        createRightfaceUpDownTriangleWith(lineNo: 10)
+        createDiamondPatternWith(lineNo: 5)
     }
 
 }
@@ -32,8 +35,59 @@ extension ViewController{
         }
         
         print("\(spaceStar)")
-
+    }
+    
+    func createRightfaceTriangleWith(lineNo : Int) -> Void{
         
+        var spaceStar : String = String()
+        for i in 0 ... lineNo{
+            for _ in 0 ... i{
+                spaceStar = spaceStar + "*"
+            }
+            spaceStar = spaceStar + "\n"
+        }
+        
+        print("\(spaceStar)")
+        
+    }
+    
+    func createRightfaceUpDownTriangleWith(lineNo : Int) -> Void{
+        
+        var spaceStar : String = String()
+        for i in 0 ... lineNo{
+            for _ in 0 ... i{
+                spaceStar = spaceStar + "*"
+            }
+            spaceStar = spaceStar + "\n"
+        }
+        for i in (0 ... lineNo).reversed(){
+           for _ in 0 ... i{
+               spaceStar = spaceStar + "*"
+           }
+           spaceStar = spaceStar + "\n"
+       }
+        
+        print("\(spaceStar)")
+        
+    }
+    
+    func createDiamondPatternWith(lineNo : Int) -> Void{
+        var spaceString : String = String()
+        for i in 1 ..< lineNo{
+            let spaceCount = (lineNo - i)/2
+            spaceString = spaceString + String(repeating: " ", count: spaceCount)
+            let count = (i - 1) * 2 + 1
+            spaceString = spaceString + String(repeating: "*", count: count)
+            spaceString = spaceString + "\n"
+        }
+        for i in (1 ... lineNo - 1).reversed(){
+            let spaceCount = i / 2
+            spaceString = spaceString + String(repeating: " ", count: spaceCount)
+           let count = (i + 1)
+           spaceString = spaceString + String(repeating: "*", count: count)
+            spaceString = spaceString + "\n"
+       }
+        print("\(spaceString)")
     }
     
 }
